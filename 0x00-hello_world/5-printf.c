96b251003485f90b085f9390996a9354384d478d
Imanesraidi <sraidiimane123@gmail.com>
2023-08-13 16:10:50 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,9 @@
-#include<studio.h>
+#include <stdio.h>
 /**
-*main - Entry point
-*Return: Always 0 (Success)
+* main - printing output with printf
+*
+* Return: always (0)
+*
 */
 int main(void)
 {
