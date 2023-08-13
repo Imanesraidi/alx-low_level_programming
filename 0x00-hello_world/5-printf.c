16f93af8db26d8d36f0876330437f0f1dbffc1ce
Imanesraidi <sraidiimane123@gmail.com>
2023-08-13 09:38:48 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+#include<studio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilinual puzzle");
+return (0);
+}#include<studio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilinual puzzle");
+return (0);
+}
